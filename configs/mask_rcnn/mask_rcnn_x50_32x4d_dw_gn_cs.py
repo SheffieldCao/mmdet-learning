e4f4a380b64ff06b215c77a3987ef94dd357d89a
SheffieldCao <1751899@tongji.edu.cn>
2022-04-15 21:09:53 +0800

    [Config] cs fintune scheme based on coco_maskrcnn

---
+++ configs/mask_rcnn/mask_rcnn_x50_32x4d_dw_gn_cs.py
@@ -0,0 +1,40 @@
+_base_ = [ './mask_rcnn_r50_fpn_cs_8x2.py']
+
+# model settings
+conv_cfg = dict(type='ConvWS')
+norm_cfg = dict(type='GN', num_groups=32, requires_grad=True)
+model = dict(
+    backbone=dict(
+        type='ResNeXt',
+        depth=50,
+        groups=32,
+        base_width=4,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        style='pytorch',
+        conv_cfg=conv_cfg,
+        norm_cfg=norm_cfg,
+        init_cfg=None
+        )
+    )
+
+# model series URL: <https://github.com/open-mmlab/mmdetection/tree/master/configs/gn%2Bws>_.
+# load_from='https://download.openmmlab.com/mmdetection/v2.0/gn%2Bws/mask_rcnn_x50_32x4d_fpn_gn_ws-all_2x_coco/mask_rcnn_x50_32x4d_fpn_gn_ws-all_2x_coco_20200216-649fdb6f.pth' #noqa
+load_from='~/mmdet/outputs/pretrained_models/mask_rcnn_x50_32x4d_fpn_gn_ws-all_2x_coco_20200216-649fdb6f.pth' #noqa
+
+# optimizer
+optimizer = dict(type='AdamW', lr=1e-3, weight_decay=5e-4)
+optimizer_config = dict()
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.1,
+    step=[20, 42, 49, 52])
+runner = dict(type='EpochBasedRunner', max_epochs=55)
+cudnn_benchmark = False
+
+# resume
+resume_from = '~/mmdet/outputs/mask_rcnn_x50_32x4d_dw_gn_cs_8x2_cs_1024/epoch_2.pth'
