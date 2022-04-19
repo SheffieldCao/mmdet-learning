13a5376198da5286eedf2a601ad3b4f78ac64f06
SheffieldCao <1751899@tongji.edu.cn>
2022-04-19 12:15:55 +0800

    [Update] maskrcnn gn-ws cfg

--- configs/mask_rcnn/mask_rcnn_x50_32x4d_dw_gn_cs.py
+++ configs/mask_rcnn/mask_rcnn_x50_32x4d_dw_gn_cs.py
@@ -16,12 +16,12 @@ model = dict(
         conv_cfg=conv_cfg,
         norm_cfg=norm_cfg,
         init_cfg=None
-        )
     )
+)
 
-# model series URL: <https://github.com/open-mmlab/mmdetection/tree/master/configs/gn%2Bws>_.
+## fintune model series URL: <https://github.com/open-mmlab/mmdetection/tree/master/configs/gn%2Bws>_.
 # load_from='https://download.openmmlab.com/mmdetection/v2.0/gn%2Bws/mask_rcnn_x50_32x4d_fpn_gn_ws-all_2x_coco/mask_rcnn_x50_32x4d_fpn_gn_ws-all_2x_coco_20200216-649fdb6f.pth' #noqa
-load_from='~/mmdet/outputs/pretrained_models/mask_rcnn_x50_32x4d_fpn_gn_ws-all_2x_coco_20200216-649fdb6f.pth' #noqa
+# load_from='~/mmdet/outputs/pretrained_models/mask_rcnn_x50_32x4d_fpn_gn_ws-all_2x_coco_20200216-649fdb6f.pth' #noqa
 
 # optimizer
 optimizer = dict(type='AdamW', lr=1e-3, weight_decay=5e-4)
@@ -37,4 +37,4 @@ runner = dict(type='EpochBasedRunner', max_epochs=55)
 cudnn_benchmark = False
 
 # resume
-resume_from = '~/mmdet/outputs/mask_rcnn_x50_32x4d_dw_gn_cs_8x2_cs_1024/epoch_2.pth'
+# resume_from = '~/mmdet/outputs/mask_rcnn_x50_32x4d_dw_gn_cs_2x2_cs_1024_from_scratch/epoch_9.pth'
