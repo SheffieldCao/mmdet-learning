a6cb8a55c3419326c2ac66d3358df3fffef1ae85
SheffieldCao <1751899@tongji.edu.cn>
2022-04-22 19:55:20 +0800

    [Fix] norm type in global propagation block

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -7,7 +7,7 @@ from .efficientnet import EfficientNet
 from .hourglass import HourglassNet
 from .hrnet import HRNet
 from .mobilenet_v2 import MobileNetV2
-from .gpvit import GPViT
+from .gpvit import GPFormer
 from .pvt import PyramidVisionTransformer, PyramidVisionTransformerV2
 from .regnet import RegNet
 from .res2net import Res2Net
@@ -23,5 +23,5 @@ __all__ = [
     'MobileNetV2', 'Res2Net', 'HourglassNet', 'DetectoRS_ResNet',
     'DetectoRS_ResNeXt', 'Darknet', 'ResNeSt', 'TridentResNet', 'CSPDarknet',
     'SwinTransformer', 'PyramidVisionTransformer',
-    'PyramidVisionTransformerV2', 'EfficientNet', 'GPViT'
+    'PyramidVisionTransformerV2', 'EfficientNet', 'GPFormer'
 ]
--- mmdet/models/backbones/gpvit.py
+++ mmdet/models/backbones/gpvit.py
@@ -6,17 +6,15 @@ from copy import deepcopy
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.utils.checkpoint as cp
 from mmcv.cnn import build_norm_layer, constant_init, trunc_normal_init
-from mmcv.cnn.bricks.transformer import FFN, build_dropout
-from mmcv.cnn.utils.weight_init import trunc_normal_, xavier_init, constant_init
+from mmcv.cnn.utils.weight_init import trunc_normal_, constant_init
 from mmcv.runner import BaseModule, ModuleList, _load_checkpoint
 from mmcv.utils import to_2tuple
 from mmcv.cnn import build_activation_layer
 
 from ...utils import get_root_logger
 from ..builder import BACKBONES
-from ..utils.transformer import nlc_to_nchw, nchw_to_nlc
+from ..utils.transformer import nlc_to_nchw, nchw_to_nlc, PatchEmbed
 from .swin import SwinBlock
 from .resnet import BasicBlock, Bottleneck
 
@@ -34,18 +32,19 @@ class BasicBlockv2(BasicBlock):
                  with_cp=False,
                  conv_cfg=None,
                  norm_cfg=dict(type='BN'),
-                 act_cfg=dict(type='ReLU',inplace=True),
+                 act_cfg=dict(type='GELU'),
                  dcn=None,
                  plugins=None,
                  init_cfg=None):
         super(BasicBlockv2, self).__init__(inplanes, planes, stride, dilation, downsample, style, 
                                            with_cp, conv_cfg, norm_cfg, dcn, plugins, init_cfg)
         self.act_cfg = act_cfg
-        if self.act_cfg.type == 'GELU':
-            assert 'in_place' not in self.act_cfg, " `GELU ` got an unexpected argument ` in_place `"
-        self.relu = build_activation_layer(self.act_cfg)
-
-        self.init_weights()
+        if self.act_cfg is not None:
+            if getattr(self.act_cfg,'type', None) == 'GELU':
+                assert 'in_place' not in self.act_cfg, " `GELU ` got an unexpected argument ` in_place `"
+            self.relu = build_activation_layer(self.act_cfg)
+        
+        assert getattr(self.norm_cfg, 'type', None) != 'LN', " ` norm_cfg ` not supports ` LN `"
 
     def init_weights(self):
         logger = get_root_logger()
@@ -67,18 +66,19 @@ class Bottleneckv2(Bottleneck):
                  with_cp=False,
                  conv_cfg=None,
                  norm_cfg=dict(type='BN'),
-                 act_cfg=dict(type='ReLU',inplace=True),
+                 act_cfg=dict(type='GELU'),
                  dcn=None,
                  plugins=None,
                  init_cfg=None):
         super(Bottleneckv2, self).__init__(inplanes, planes, stride, dilation, downsample, style, with_cp,
                                             conv_cfg, norm_cfg, dcn, plugins, init_cfg)
         self.act_cfg = act_cfg
-        if self.act_cfg.type == 'GELU':
-            assert 'in_place' not in self.act_cfg, " `GELU ` got an unexpected argument ` in_place `"
-        self.relu = build_activation_layer(self.act_cfg)
-        
-        self.init_weights()
+        if self.act_cfg is not None:
+            if getattr(self.act_cfg,'type', None) == 'GELU':
+                assert 'in_place' not in self.act_cfg, " `GELU ` got an unexpected argument ` in_place `"
+            self.relu = build_activation_layer(self.act_cfg)
+
+        assert getattr(self.norm_cfg, 'type', None) != 'LN', " ` norm_cfg ` not supports ` LN `"
 
     def init_weights(self):
         logger = get_root_logger()
@@ -128,7 +128,7 @@ class GPFormerSequence(BaseModule):
                  attn_drop_rate=0.,
                  drop_path_rate=0.,
                  gp_conv_cfg=None,
-                 gp_norm_cfg=dict(type='LN'),
+                 gp_norm_cfg=dict(type='BN'),
                  gp_act_cfg=dict(type='GELU'),
                  act_cfg=dict(type='GELU'),
                  norm_cfg=dict(type='LN'),
@@ -179,21 +179,23 @@ class GPFormerSequence(BaseModule):
         x = nchw_to_nlc(self.global_propagation(nlc_to_nchw(x, hw_shape)))
         assert (x.size()[2], x.size()[3]) == hw_shape, "ResBlock output shape is not equal to input shape"
 
-        return x, hw_shape
+        return x, hw_shape, x, hw_shape
 
 
-class GPViT(BaseModule):
+@BACKBONES.register_module()
+class GPFormer(BaseModule):
+    """
+    
+    """
     def __init__(self,
                  pretrain_img_size=224,
                  in_channels=3,
-                 embed_dims=96,
-                 patch_size=4,
-                 window_size=7,
+                 embed_dims=768,
+                 patch_size=16,
+                 window_size=16,
                  mlp_ratio=4,
                  depths=(3, 3, 3, 3),
-                 num_heads=(3, 6, 12, 24),
-                 strides=(4, 2, 2, 2),
-                 out_indices=(0, 1, 2, 3),
+                 num_heads=(12, 12, 12, 12),
                  qkv_bias=True,
                  qk_scale=None,
                  patch_norm=True,
@@ -201,6 +203,9 @@ class GPViT(BaseModule):
                  attn_drop_rate=0.,
                  drop_path_rate=0.1,
                  use_abs_pos_embed=False,
+                 gp_conv_cfg=dict(type='ConvWS'),
+                 gp_norm_cfg=dict(type='BN'),
+                 gp_act_cfg=dict(type='GELU'),
                  act_cfg=dict(type='GELU'),
                  norm_cfg=dict(type='LN'),
                  with_cp=False,
@@ -230,20 +235,17 @@ class GPViT(BaseModule):
         else:
             raise TypeError('pretrained must be a str or None')
 
-        super(SwinTransformer, self).__init__(init_cfg=init_cfg)
+        super(GPFormer, self).__init__(init_cfg=init_cfg)
 
         num_layers = len(depths)
-        self.out_indices = out_indices
         self.use_abs_pos_embed = use_abs_pos_embed
-
-        assert strides[0] == patch_size, 'Use non-overlapping patch embed.'
 
         self.patch_embed = PatchEmbed(
             in_channels=in_channels,
             embed_dims=embed_dims,
             conv_type='Conv2d',
             kernel_size=patch_size,
-            stride=strides[0],
+            stride=patch_size,
             norm_cfg=norm_cfg if patch_norm else None,
             init_cfg=None)
 
@@ -263,22 +265,11 @@ class GPViT(BaseModule):
         ]
 
         self.stages = ModuleList()
-        in_channels = embed_dims
         for i in range(num_layers):
-            if i < num_layers - 1:
-                downsample = PatchMerging(
-                    in_channels=in_channels,
-                    out_channels=2 * in_channels,
-                    stride=strides[i + 1],
-                    norm_cfg=norm_cfg if patch_norm else None,
-                    init_cfg=None)
-            else:
-                downsample = None
-
-            stage = SwinBlockSequence(
-                embed_dims=in_channels,
+            stage = GPFormerSequence(
+                embed_dims=embed_dims,
                 num_heads=num_heads[i],
-                feedforward_channels=mlp_ratio * in_channels,
+                feedforward_channels=mlp_ratio * embed_dims,
                 depth=depths[i],
                 window_size=window_size,
                 qkv_bias=qkv_bias,
@@ -286,25 +277,22 @@ class GPViT(BaseModule):
                 drop_rate=drop_rate,
                 attn_drop_rate=attn_drop_rate,
                 drop_path_rate=dpr[sum(depths[:i]):sum(depths[:i + 1])],
-                downsample=downsample,
+                gp_conv_cfg=gp_conv_cfg,
+                gp_norm_cfg=gp_norm_cfg,
+                gp_act_cfg=gp_act_cfg,
                 act_cfg=act_cfg,
                 norm_cfg=norm_cfg,
                 with_cp=with_cp,
                 init_cfg=None)
             self.stages.append(stage)
-            if downsample:
-                in_channels = downsample.out_channels
 
-        self.num_features = [int(embed_dims * 2**i) for i in range(num_layers)]
-        # Add a norm layer for each output
-        for i in out_indices:
-            layer = build_norm_layer(norm_cfg, self.num_features[i])[1]
-            layer_name = f'norm{i}'
-            self.add_module(layer_name, layer)
+        self.num_features = embed_dims
+        layer = build_norm_layer(norm_cfg, self.num_features)[1]
+        self.add_module(f'final_norm', layer)
 
     def train(self, mode=True):
         """Convert the model into training mode while keep layers freezed."""
-        super(SwinTransformer, self).train(mode)
+        super(GPFormer, self).train(mode)
         self._freeze_stages()
 
     def _freeze_stages(self):
@@ -318,16 +306,16 @@ class GPViT(BaseModule):
 
         for i in range(1, self.frozen_stages + 1):
 
-            if (i - 1) in self.out_indices:
-                norm_layer = getattr(self, f'norm{i-1}')
-                norm_layer.eval()
-                for param in norm_layer.parameters():
-                    param.requires_grad = False
-
             m = self.stages[i - 1]
             m.eval()
             for param in m.parameters():
                 param.requires_grad = False
+        
+        # only remain the last norm for output layer
+        norm_layer = getattr(self, f'final_norm')
+        norm_layer.eval()
+        for param in norm_layer.parameters():
+            param.requires_grad = False
 
     def init_weights(self):
         logger = get_root_logger()
@@ -343,66 +331,68 @@ class GPViT(BaseModule):
                 elif isinstance(m, nn.LayerNorm):
                     constant_init(m, 1.0)
         else:
-            assert 'checkpoint' in self.init_cfg, f'Only support ' \
-                                                  f'specify `Pretrained` in ' \
-                                                  f'`init_cfg` in ' \
-                                                  f'{self.__class__.__name__} '
-            ckpt = _load_checkpoint(
-                self.init_cfg.checkpoint, logger=logger, map_location='cpu')
-            if 'state_dict' in ckpt:
-                _state_dict = ckpt['state_dict']
-            elif 'model' in ckpt:
-                _state_dict = ckpt['model']
-            else:
-                _state_dict = ckpt
-            if self.convert_weights:
-                # supported loading weight from original repo,
-                _state_dict = swin_converter(_state_dict)
+            # TODO: load pretrain Swin Transformer Block weights
+            pass
+            # assert 'checkpoint' in self.init_cfg, f'Only support ' \
+            #                                       f'specify `Pretrained` in ' \
+            #                                       f'`init_cfg` in ' \
+            #                                       f'{self.__class__.__name__} '
+            # ckpt = _load_checkpoint(
+            #     self.init_cfg.checkpoint, logger=logger, map_location='cpu')
+            # if 'state_dict' in ckpt:
+            #     _state_dict = ckpt['state_dict']
+            # elif 'model' in ckpt:
+            #     _state_dict = ckpt['model']
+            # else:
+            #     _state_dict = ckpt
+            # if self.convert_weights:
+            #     # supported loading weight from original repo,
+            #     _state_dict = swin_converter(_state_dict)
 
-            state_dict = OrderedDict()
-            for k, v in _state_dict.items():
-                if k.startswith('backbone.'):
-                    state_dict[k[9:]] = v
+            # state_dict = OrderedDict()
+            # for k, v in _state_dict.items():
+            #     if k.startswith('backbone.'):
+            #         state_dict[k[9:]] = v
 
-            # strip prefix of state_dict
-            if list(state_dict.keys())[0].startswith('module.'):
-                state_dict = {k[7:]: v for k, v in state_dict.items()}
+            # # strip prefix of state_dict
+            # if list(state_dict.keys())[0].startswith('module.'):
+            #     state_dict = {k[7:]: v for k, v in state_dict.items()}
 
-            # reshape absolute position embedding
-            if state_dict.get('absolute_pos_embed') is not None:
-                absolute_pos_embed = state_dict['absolute_pos_embed']
-                N1, L, C1 = absolute_pos_embed.size()
-                N2, C2, H, W = self.absolute_pos_embed.size()
-                if N1 != N2 or C1 != C2 or L != H * W:
-                    logger.warning('Error in loading absolute_pos_embed, pass')
-                else:
-                    state_dict['absolute_pos_embed'] = absolute_pos_embed.view(
-                        N2, H, W, C2).permute(0, 3, 1, 2).contiguous()
+            # # reshape absolute position embedding
+            # if state_dict.get('absolute_pos_embed') is not None:
+            #     absolute_pos_embed = state_dict['absolute_pos_embed']
+            #     N1, L, C1 = absolute_pos_embed.size()
+            #     N2, C2, H, W = self.absolute_pos_embed.size()
+            #     if N1 != N2 or C1 != C2 or L != H * W:
+            #         logger.warning('Error in loading absolute_pos_embed, pass')
+            #     else:
+            #         state_dict['absolute_pos_embed'] = absolute_pos_embed.view(
+            #             N2, H, W, C2).permute(0, 3, 1, 2).contiguous()
 
-            # interpolate position bias table if needed
-            relative_position_bias_table_keys = [
-                k for k in state_dict.keys()
-                if 'relative_position_bias_table' in k
-            ]
-            for table_key in relative_position_bias_table_keys:
-                table_pretrained = state_dict[table_key]
-                table_current = self.state_dict()[table_key]
-                L1, nH1 = table_pretrained.size()
-                L2, nH2 = table_current.size()
-                if nH1 != nH2:
-                    logger.warning(f'Error in loading {table_key}, pass')
-                elif L1 != L2:
-                    S1 = int(L1**0.5)
-                    S2 = int(L2**0.5)
-                    table_pretrained_resized = F.interpolate(
-                        table_pretrained.permute(1, 0).reshape(1, nH1, S1, S1),
-                        size=(S2, S2),
-                        mode='bicubic')
-                    state_dict[table_key] = table_pretrained_resized.view(
-                        nH2, L2).permute(1, 0).contiguous()
+            # # interpolate position bias table if needed
+            # relative_position_bias_table_keys = [
+            #     k for k in state_dict.keys()
+            #     if 'relative_position_bias_table' in k
+            # ]
+            # for table_key in relative_position_bias_table_keys:
+            #     table_pretrained = state_dict[table_key]
+            #     table_current = self.state_dict()[table_key]
+            #     L1, nH1 = table_pretrained.size()
+            #     L2, nH2 = table_current.size()
+            #     if nH1 != nH2:
+            #         logger.warning(f'Error in loading {table_key}, pass')
+            #     elif L1 != L2:
+            #         S1 = int(L1**0.5)
+            #         S2 = int(L2**0.5)
+            #         table_pretrained_resized = F.interpolate(
+            #             table_pretrained.permute(1, 0).reshape(1, nH1, S1, S1),
+            #             size=(S2, S2),
+            #             mode='bicubic')
+            #         state_dict[table_key] = table_pretrained_resized.view(
+            #             nH2, L2).permute(1, 0).contiguous()
 
-            # load state_dict
-            self.load_state_dict(state_dict, False)
+            # # load state_dict
+            # self.load_state_dict(state_dict, False)
 
     def forward(self, x):
         x, hw_shape = self.patch_embed(x)
@@ -411,15 +401,13 @@ class GPViT(BaseModule):
             x = x + self.absolute_pos_embed
         x = self.drop_after_pos(x)
 
-        outs = []
-        for i, stage in enumerate(self.stages):
+        for stage in self.stages:
             x, hw_shape, out, out_hw_shape = stage(x, hw_shape)
-            if i in self.out_indices:
-                norm_layer = getattr(self, f'norm{i}')
-                out = norm_layer(out)
-                out = out.view(-1, *out_hw_shape,
-                               self.num_features[i]).permute(0, 3, 1,
-                                                             2).contiguous()
-                outs.append(out)
 
-        return outs
+
+        norm_layer = getattr(self, f'final_norm')
+        out = norm_layer(out)
+        out = out.view(-1, *out_hw_shape, 
+                    self.num_features).permute(0, 3, 1, 2).contiguous()
+
+        return out
