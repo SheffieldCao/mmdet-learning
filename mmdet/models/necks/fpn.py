5ebf25f95c186498d5be462294f37992e3a8204c
SheffieldCao <1751899@tongji.edu.cn>
2022-04-23 11:15:46 +0800

    [Feature] SFP formal implements

--- mmdet/models/necks/fpn.py
+++ mmdet/models/necks/fpn.py
@@ -1,11 +1,14 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import warnings
+import math
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from mmcv.cnn import ConvModule
 from mmcv.runner import BaseModule, auto_fp16
+from mmcv.cnn import build_activation_layer, build_norm_layer, build_conv_layer
 
 from ..builder import NECKS
-
+from ..utils.transformer import nlc_to_nchw, nchw_to_nlc
 
 @NECKS.register_module()
 class FPN(BaseModule):
@@ -203,8 +206,19 @@ class FPN(BaseModule):
                         outs.append(self.fpn_convs[i](outs[-1]))
         return tuple(outs)
 
-class ConvTransModule(ConvModule):
-    """A deconv block that bundles conv/norm/activation layers.
+class ConvModule(nn.Module):
+    """A conv block that bundles conv/norm/activation layers.
+
+    This block simplifies the usage of convolution layers, which are commonly
+    used with a norm layer (e.g., BatchNorm) and activation layer (e.g., ReLU).
+    It is based upon three build methods: `build_conv_layer()`,
+    `build_norm_layer()` and `build_activation_layer()`.
+
+    Besides, we add some additional features in this module.
+    1. Automatically set `bias` of the conv layer.
+    2. Spectral norm is supported.
+    3. More padding modes are supported. Before PyTorch 1.5, nn.Conv2d only
+    supports zero and circular padding, and we add "reflect" padding mode.
 
     Args:
         in_channels (int): Number of channels in the input feature map.
@@ -241,13 +255,15 @@ class ConvTransModule(ConvModule):
             ("conv", "norm", "act") and ("act", "conv", "norm").
             Default: ('conv', 'norm', 'act').
     """
+
+    _abbr_ = 'conv_block'
+
     def __init__(self,
                  in_channels,
                  out_channels,
                  kernel_size,
-                 stride=2,
-                 padding=1,
-                 output_padding=1,
+                 stride=1,
+                 padding=0,
                  dilation=1,
                  groups=1,
                  bias='auto',
@@ -255,10 +271,42 @@ class ConvTransModule(ConvModule):
                  norm_cfg=None,
                  act_cfg=dict(type='ReLU'),
                  inplace=True,
+                 padding_mode='zeros',
                  order=('conv', 'norm', 'act')):
-        super(ConvTransModule, self).__init__(in_channels, out_channels, kernel_size, padding=padding, groups=groups, bias=bias, conv_cfg=conv_cfg, norm_cfg=norm_cfg, act_cfg=act_cfg, inplace=inplace, order=order)
-        self.conv = nn.ConvTranspose2d(
-                in_channels, out_channels, kernel_size, stride, self.padding, output_padding=output_padding, groups=self.groups, bias=self.with_bias, dilation=dilation)
+        super(ConvModule, self).__init__()
+        assert conv_cfg is None or isinstance(conv_cfg, dict)
+        assert norm_cfg is None or isinstance(norm_cfg, dict)
+        assert act_cfg is None or isinstance(act_cfg, dict)
+        official_padding_mode = ['zeros', 'circular']
+        assert padding_mode in official_padding_mode
+        self.conv_cfg = conv_cfg
+        self.norm_cfg = norm_cfg
+        self.act_cfg = act_cfg
+        self.inplace = inplace
+        self.order = order
+        assert isinstance(self.order, tuple) and len(self.order) == 3
+        assert set(order) == set(['conv', 'norm', 'act'])
+
+        self.with_norm = norm_cfg is not None
+        self.with_activation = act_cfg is not None
+        # if the conv layer is before a norm layer, bias is unnecessary.
+        if bias == 'auto':
+            bias = not self.with_norm
+        self.with_bias = bias
+
+        # reset padding to 0 for conv module
+        conv_padding = 0 if self.with_explicit_padding else padding
+        # build convolution layer
+        self.conv = build_conv_layer(
+            conv_cfg,
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride=stride,
+            padding=conv_padding,
+            dilation=dilation,
+            groups=groups,
+            bias=bias)
         # export the attributes of self.conv to a higher level for convenience
         self.in_channels = self.conv.in_channels
         self.out_channels = self.conv.out_channels
@@ -268,6 +316,260 @@ class ConvTransModule(ConvModule):
         self.dilation = self.conv.dilation
         self.transposed = self.conv.transposed
         self.output_padding = self.conv.output_padding
+        self.groups = self.conv.groups
+
+        if self.with_spectral_norm:
+            self.conv = nn.utils.spectral_norm(self.conv)
+
+        # build normalization layers
+        if self.with_norm:
+            from mmcv.utils import _BatchNorm, _InstanceNorm
+            # norm layer is after conv layer
+            if order.index('norm') > order.index('conv'):
+                norm_channels = out_channels
+            else:
+                norm_channels = in_channels
+            self.norm_name, norm = build_norm_layer(norm_cfg, norm_channels)
+            self.add_module(self.norm_name, norm)
+            if self.with_bias:
+                if isinstance(norm, (_BatchNorm, _InstanceNorm)):
+                    warnings.warn(
+                        'Unnecessary conv bias before batch/instance norm')
+        else:
+            self.norm_name = None
+
+        # build activation layer
+        if self.with_activation:
+            act_cfg_ = act_cfg.copy()
+            # nn.Tanh has no 'inplace' argument
+            if act_cfg_['type'] not in [
+                    'Tanh', 'PReLU', 'Sigmoid', 'HSigmoid', 'Swish', 'GELU'
+            ]:
+                act_cfg_.setdefault('inplace', inplace)
+            self.activate = build_activation_layer(act_cfg_)
+
+        # Use msra init by default
+        self.init_weights()
+
+    @property
+    def norm(self):
+        if self.norm_name:
+            return getattr(self, self.norm_name)
+        else:
+            return None
+
+    def init_weights(self):
+        # 1. It is mainly for customized conv layers with their own
+        #    initialization manners by calling their own ``init_weights()``,
+        #    and we do not want ConvModule to override the initialization.
+        # 2. For customized conv layers without their own initialization
+        #    manners (that is, they don't have their own ``init_weights()``)
+        #    and PyTorch's conv layers, they will be initialized by
+        #    this method with default ``kaiming_init``.
+        # Note: For PyTorch's conv layers, they will be overwritten by our
+        #    initialization implementation using default ``kaiming_init``.
+        from mmcv.cnn.utils.weight_init import kaiming_init, constant_init
+        if not hasattr(self.conv, 'init_weights'):
+            if self.with_activation and self.act_cfg['type'] == 'LeakyReLU':
+                nonlinearity = 'leaky_relu'
+                a = self.act_cfg.get('negative_slope', 0.01)
+            else:
+                nonlinearity = 'relu'
+                a = 0
+            kaiming_init(self.conv, a=a, nonlinearity=nonlinearity)
+        if self.with_norm:
+            constant_init(self.norm, 1, bias=0)
+
+    def forward(self, x, activate=True, norm=True):
+        for layer in self.order:
+            if layer == 'conv':
+                if self.with_explicit_padding:
+                    x = self.padding_layer(x)
+                x = self.conv(x)
+            elif layer == 'norm' and norm and self.with_norm:
+                if getattr(self.norm_cfg, 'type') == 'LN':
+                    x = nchw_to_nlc(x)
+                    hw_shape = x.size()[2:]
+                x = self.norm(x)
+                if getattr(self.norm_cfg, 'type') == 'LN':
+                    x = nlc_to_nchw(x, hw_shape)
+            elif layer == 'act' and activate and self.with_activation:
+                x = self.activate(x)
+        return x
+
+class DeConvModule(BaseModule):
+    """A deconv block bundles conv/norm/activation layers.
+    
+    Args:
+        in_channels (int): Number of channels in the input feature map.
+            Same as that in ``nn._ConvNd``.
+        out_channels (int): Number of channels produced by the convolution.
+            Same as that in ``nn._ConvNd``.
+        kernel_size (int | tuple[int]): Size of the convolving kernel.
+            Same as that in ``nn._ConvNd``.
+        stride (int | tuple[int]): Stride of the convolution.
+            Same as that in ``nn._ConvNd``.
+        padding (int | tuple[int]): Zero-padding added to both sides of
+            the input. Same as that in ``nn._ConvNd``.
+        dilation (int | tuple[int]): Spacing between kernel elements.
+            Same as that in ``nn._ConvNd``.
+        groups (int): Number of blocked connections from input channels to
+            output channels. Same as that in ``nn._ConvNd``.
+        bias (bool | str): If specified as `auto`, it will be decided by the
+            norm_cfg. Bias will be set as True if `norm_cfg` is None, otherwise
+            False. Default: "auto".
+        conv_cfg (dict): Config dict for convolution layer. Default: None,
+            which means using conv2d.
+        norm_cfg (dict): Config dict for normalization layer. Default: None.
+        act_cfg (dict): Config dict for activation layer.
+            Default: dict(type='ReLU').
+        inplace (bool): Whether to use inplace mode for activation.
+            Default: True.
+        with_spectral_norm (bool): Whether use spectral norm in conv module.
+            Default: False.
+        padding_mode (str): If the `padding_mode` has not been supported by
+            current `Conv2d` in PyTorch, we will use our own padding layer
+            instead. Currently, we support ['zeros', 'circular'] with official
+            implementation and ['reflect'] with our own implementation.
+            Default: 'zeros'.
+        order (tuple[str]): The order of conv/norm/activation layers. It is a
+            sequence of "conv", "norm" and "act". Common examples are
+            ("conv", "norm", "act") and ("act", "conv", "norm").
+            Default: ('conv', 'norm', 'act').
+    """
+
+    _abbr_ = 'conv_block'
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 padding=0,
+                 output_padding=0,
+                 dilation=1,
+                 groups=1,
+                 with_bias=False,
+                 conv_cfg=None,
+                 norm_cfg=dict(type='LN'),
+                 act_cfg=dict(type='GELU'),
+                 inplace=True,
+                 padding_mode='zeros',
+                 order=('conv', 'norm', 'act')):
+        super(ConvModule, self).__init__()
+        assert conv_cfg is None or isinstance(conv_cfg, dict)
+        assert norm_cfg is None or isinstance(norm_cfg, dict)
+        assert act_cfg is None or isinstance(act_cfg, dict)
+        official_padding_mode = ['zeros', 'circular']
+        self.conv_cfg = conv_cfg
+        self.norm_cfg = norm_cfg
+        self.act_cfg = act_cfg
+        self.inplace = inplace
+        assert padding_mode in official_padding_mode, "only support {0}, {1} padding mode".format('zeros', 'circular')
+        self.order = order
+        assert isinstance(self.order, tuple) and len(self.order) == 3
+        assert set(order) == set(['conv', 'norm', 'act'])
+        self.groups = groups
+        self.output_padding = output_padding
+        self.with_norm = norm_cfg is not None
+        self.with_activation = act_cfg is not None
+
+        # if the conv layer is before a norm layer, bias is unnecessary.
+        self.with_bias = with_bias
+        self.with_bias = not self.with_norm
+
+        # build convolution layer
+        self.conv = nn.ConvTranspose2d(
+                in_channels, out_channels, kernel_size, stride, self.padding, output_padding=self.output_padding, 
+                groups=self.groups, bias=self.with_bias, dilation=dilation, padding_mode=padding_mode)
+
+        # export the attributes of self.conv to a higher level for convenience
+        self.in_channels = self.conv.in_channels
+        self.out_channels = self.conv.out_channels
+        self.kernel_size = self.conv.kernel_size
+        self.stride = self.conv.stride
+        self.padding = padding
+        self.dilation = self.conv.dilation
+        self.transposed = self.conv.transposed
+        self.output_padding = self.conv.output_padding
+        self.groups = self.conv.groups
+
+        # build normalization layers
+        if self.with_norm:
+            from mmcv.utils import _BatchNorm, _InstanceNorm
+            # norm layer is after conv layer
+            if order.index('norm') > order.index('conv'):
+                norm_channels = out_channels
+            else:
+                norm_channels = in_channels
+            self.norm_name, norm = build_norm_layer(norm_cfg, norm_channels)
+            self.add_module(self.norm_name, norm)
+            if self.with_bias:
+                if isinstance(norm, (_BatchNorm, _InstanceNorm)):
+                    warnings.warn(
+                        'Unnecessary conv bias before batch/instance norm')
+        else:
+            self.norm_name = None
+
+        # build activation layer
+        if self.with_activation:
+            act_cfg_ = act_cfg.copy()
+            # nn.Tanh has no 'inplace' argument
+            if act_cfg_['type'] not in [
+                    'Tanh', 'PReLU', 'Sigmoid', 'HSigmoid', 'Swish', 'GELU'
+            ]:
+                act_cfg_.setdefault('inplace', inplace)
+            self.activate = build_activation_layer(act_cfg_)
+
+        # Use msra init by default
+        self.init_weights()
+
+    @property
+    def norm(self):
+        if self.norm_name:
+            return getattr(self, self.norm_name)
+        else:
+            return None
+
+    def init_weights(self):
+        # 1. It is mainly for customized conv layers with their own
+        #    initialization manners by calling their own ``init_weights()``,
+        #    and we do not want ConvModule to override the initialization.
+        # 2. For customized conv layers without their own initialization
+        #    manners (that is, they don't have their own ``init_weights()``)
+        #    and PyTorch's conv layers, they will be initialized by
+        #    this method with default ``kaiming_init``.
+        # Note: For PyTorch's conv layers, they will be overwritten by our
+        #    initialization implementation using default ``kaiming_init``.
+        from mmcv.cnn.utils.weight_init import kaiming_init, constant_init
+        if not hasattr(self.conv, 'init_weights'):
+            if self.with_activation and self.act_cfg['type'] == 'LeakyReLU':
+                nonlinearity = 'leaky_relu'
+                a = self.act_cfg.get('negative_slope', 0.01)
+            else:
+                nonlinearity = 'relu'
+                a = 0
+            kaiming_init(self.conv, a=a, nonlinearity=nonlinearity)
+        if self.with_norm:
+            constant_init(self.norm, 1, bias=0)
+
+    def forward(self, x, activate=True, norm=True):
+        for layer in self.order:
+            if layer == 'conv':
+                if self.with_explicit_padding:
+                    x = self.padding_layer(x)
+                x = self.conv(x)
+            elif layer == 'norm' and norm and self.with_norm:
+                if getattr(self.norm_cfg, 'type') == 'LN':
+                    x = nchw_to_nlc(x)
+                    hw_shape = x.size()[2:]
+                x = self.norm(x)
+                if getattr(self.norm_cfg, 'type') == 'LN':
+                    x = nlc_to_nchw(x, hw_shape)
+            elif layer == 'act' and activate and self.with_activation:
+                x = self.activate(x)
+        return x
+
 
 @NECKS.register_module()
 class SFP(BaseModule):
@@ -296,6 +598,7 @@ class SFP(BaseModule):
                  strides=[2,1,1/2,1/4],
                  no_norm_on_lateral=False,
                  conv_cfg=None,
+                 deconv_norm_cfg=None,
                  norm_cfg=None,
                  act_cfg=None,
                  init_cfg=dict(
@@ -310,29 +613,24 @@ class SFP(BaseModule):
         self.lateral_convs = nn.ModuleList()
 
         for stride in strides:
-            if stride >= 1:
-                l_conv = ConvModule(
-                    in_channel,
-                    out_channels,
-                    3,
-                    stride=stride,
-                    padding=1,
-                    conv_cfg=conv_cfg,
-                    norm_cfg=norm_cfg if not self.no_norm_on_lateral else None,
-                    act_cfg=act_cfg,
-                    inplace=False)
+            l_conv = nn.ModuleList()
+            if stride > 1:
+                assert stride == 2, 'only support stride == 2.'
+                l_conv.append(nn.MaxPool2d(kernel_size=2, stride=2))
+            elif stride == 1:
+                l_conv = nn.ModuleList()
             else:
-                l_conv = ConvTransModule(
-                    in_channel,
-                    out_channels,
-                    3,
-                    stride=int(1/stride),
-                    padding=1,
-                    output_padding=int(1/stride)-1,
-                    conv_cfg=conv_cfg,
-                    norm_cfg=norm_cfg if not self.no_norm_on_lateral else None,
-                    act_cfg=act_cfg,
-                    inplace=False
+                for _ in range(int(math.log(1/stride, 2))):
+                    l_conv.append(
+                        DeConvModule(self.in_channel, self.in_channel, kernel_size=3, stride=1, padding=1, output_padding=1,  
+                            conv_cfg=conv_cfg, norm_cfg=deconv_norm_cfg, act_cfg=act_cfg, inplace=False, order=('conv', 'norm', 'act'))
+                    )
+
+            l_conv.append(
+                    [
+                        ConvModule(self.in_channel, self.out_channels, 1, conv_cfg=conv_cfg, act_cfg=None, inplace=False),
+                        ConvModule(self.out_channels, self.out_channels, 3, padding=1, conv_cfg=conv_cfg, norm_cfg=norm_cfg, act_cfg=None, inplace=False),
+                    ]
                 )
 
             self.lateral_convs.append(l_conv)
@@ -343,7 +641,6 @@ class SFP(BaseModule):
 
         # build outputs(top down results)
         outs = [
-            lateral_conv(inputs[0])
-            for lateral_conv in self.lateral_convs
+            lateral_conv(inputs[0]) for lateral_conv in self.lateral_convs
         ]
         return tuple(outs)
