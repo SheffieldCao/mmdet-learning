283da3c6af57369ccbd9fc2a84bbac169c073f72
SheffieldCao <1751899@tongji.edu.cn>
2022-04-30 19:28:50 +0800

    [Feature] add dcnv2 r50 backbone

--- configs/mask_rcnn/mask_rcnn_r50_dcnv2_ws_gn_ohem_cs.py
+++ configs/mask_rcnn/mask_rcnn_r50_dcnv2_ws_gn_ohem_cs.py
@@ -5,16 +5,10 @@ conv_cfg = dict(type='ConvWS')
 norm_cfg = dict(type='GN', num_groups=32, requires_grad=True)
 model = dict(
     backbone=dict(
-        type='ResNeXt',
-        depth=50,
-        groups=32,
-        base_width=4,
-        num_stages=4,
-        out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
-        style='pytorch',
-        conv_cfg=conv_cfg,
-        norm_cfg=norm_cfg,
+        type='ResNet',
+        frozen_stages=-1,
+        dcn=dict(type='DCNv2', deform_groups=1, fallback_on_stride=False),
+        stage_with_dcn=(False, True, True, True),
         init_cfg=None
     ),
     train_cfg=dict(
@@ -28,7 +22,7 @@ model = dict(
 
 ## fintune model series URL: <https://github.com/open-mmlab/mmdetection/tree/master/configs/gn%2Bws>_.
 # load_from='https://download.openmmlab.com/mmdetection/v2.0/gn%2Bws/mask_rcnn_x50_32x4d_fpn_gn_ws-all_2x_coco/mask_rcnn_x50_32x4d_fpn_gn_ws-all_2x_coco_20200216-649fdb6f.pth' #noqa
-load_from='~/mmdet/outputs/mask_rcnn_x50_32x4d_ws_gn_cs_8x2_cs_1024/epoch_29.pth' #noqa
+# load_from='~/mmdet/outputs/pretrained_models/mask_rcnn_x50_32x4d_fpn_gn_ws-all_2x_coco_20200216-649fdb6f.pth' #noqa
 
 # optimizer
 optimizer = dict(type='AdamW', lr=1e-3, weight_decay=5e-2)
@@ -39,10 +33,10 @@ lr_config = dict(
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=0.1,
-    gamma=0.2,
-    step=[15, 26])
-runner = dict(type='EpochBasedRunner', max_epochs=30)
+    step=[30, 60, 75],
+    gamma=0.2)
+runner = dict(type='EpochBasedRunner', max_epochs=80)
 cudnn_benchmark = False
 
 # resume
-resume_from = '~/mmdet/outputs/mask_rcnn_x50_32x4d_ws_gn_ohem_finetune_cs_4x2_1024_20e/epoch_24.pth'
+# resume_from = '~/mmdet/outputs/mask_rcnn_x50_32x4d_dw_gn_cs_2x2_cs_1024_from_scratch/epoch_9.pth'
