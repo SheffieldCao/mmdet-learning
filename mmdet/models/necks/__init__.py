7fee85cca12596b6eca917d8312ce7b6d2674f4d
SheffieldCao <1751899@tongji.edu.cn>
2022-05-30 09:52:23 +0800

    [Add] DAFPN

--- mmdet/models/necks/__init__.py
+++ mmdet/models/necks/__init__.py
@@ -5,7 +5,7 @@ from .ct_resnet_neck import CTResNetNeck
 from .dilated_encoder import DilatedEncoder
 from .dyhead import DyHead
 from .fpg import FPG
-from .fpn import FPN, SFP, CSAFPN
+from .fpn import FPN, SFP, CSAFPN, DAFPN
 from .fpn_carafe import FPN_CARAFE
 from .hrfpn import HRFPN
 from .nas_fpn import NASFPN
@@ -17,7 +17,7 @@ from .yolo_neck import YOLOV3Neck
 from .yolox_pafpn import YOLOXPAFPN
 
 __all__ = [
-    'FPN', 'SFP', 'CSAFPN', 'BFP', 'ChannelMapper', 'HRFPN', 'NASFPN', 'FPN_CARAFE', 'PAFPN',
+    'FPN', 'SFP', 'CSAFPN', 'DAFPN', 'BFP', 'ChannelMapper', 'HRFPN', 'NASFPN', 'FPN_CARAFE', 'PAFPN',
     'NASFCOS_FPN', 'RFP', 'YOLOV3Neck', 'FPG', 'DilatedEncoder',
     'CTResNetNeck', 'SSDNeck', 'YOLOXPAFPN', 'DyHead'
 ]
