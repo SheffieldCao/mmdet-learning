9bac9076b969ecc35e0cdc4f950d87d150df6750
SheffieldCao <1751899@tongji.edu.cn>
2022-06-01 18:39:45 +0800

    Fix backbone init

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -15,7 +15,7 @@ from .resnest import ResNeSt
 from .resnet import ResNet, ResNetV1d
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
-from .swin import SwinTransformer
+from .swin import SwinTransformer, SwinTransformer_
 from .trident_resnet import TridentResNet
 
 __all__ = [
@@ -23,5 +23,5 @@ __all__ = [
     'MobileNetV2', 'Res2Net', 'HourglassNet', 'DetectoRS_ResNet',
     'DetectoRS_ResNeXt', 'Darknet', 'ResNeSt', 'TridentResNet', 'CSPDarknet',
     'SwinTransformer', 'PyramidVisionTransformer',
-    'PyramidVisionTransformerV2', 'EfficientNet', 'GPWin'
+    'PyramidVisionTransformerV2', 'EfficientNet', 'GPWin', 'SwinTransformer_'
 ]
